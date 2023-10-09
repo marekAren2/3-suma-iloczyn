c3020d8e7779d82d315248785dc97aed1b2b3a00
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-09 20:11:58 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+arr=[1, 2, 3, 4, 5, 6]
+
+function sumaTablicy(arr) {
+  arr.fore
+} 
+
+// Napisz funkcję ze zmiennymi lokalnymi, która policzy sumę i iloczyn elementów tablicy [1, 2, 3, 4, 5, 6]. Funkcja przyjmuje jeden parametr - ww. tablicę z liczbami. Funkcja nie zwraca nic. Funkcja wyświetla wyniki w konsoli.
+
+// Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+
+// Podpowiedź przed wysłaniem zadania do sprawdzenia:
+// - użyj pętli for lub forEach w zadaniu
