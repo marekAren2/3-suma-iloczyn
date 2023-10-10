bd8b29e5772d29b145cef1ba0e00eaacca047123
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-10 16:28:24 +0000

    iloczyn suma tablicy forEach last

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-arr=[1, 2, 3, 4, 5, 6]
+arr = [1, 2, 3, 4, 5, 6];
 
-function sumaTablicy(arr) {
-  arr.fore
-} 
+
+function wyswietlIloczynOrazSume(arr) {
+
+  let suma = 0;
+  let iloczyn = 1;
+
+  arr.forEach(function(element) {
+    suma += element;
+    iloczyn *= element;
+  });  
+    
+  console.log('suma liczb w tabeli: ',suma);
+  console.log('iloczyn liczb w tabeli: ',iloczyn);
+  
+};
+wyswietlIloczynOrazSume(arr);
+
+// function sumaTablicy(arr) {
+//   arr.foreach()
+// } 
 
 // Napisz funkcję ze zmiennymi lokalnymi, która policzy sumę i iloczyn elementów tablicy [1, 2, 3, 4, 5, 6]. Funkcja przyjmuje jeden parametr - ww. tablicę z liczbami. Funkcja nie zwraca nic. Funkcja wyświetla wyniki w konsoli.
 
